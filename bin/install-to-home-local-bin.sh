4b6a5d79a35f12ababbf29796e75c3da6fc11f46
fluxfw <fluxfw@fluxlabs.ch>
2022-03-07 14:18:40 +0100

    fix

--- bin/install-to-home-local-bin.sh
+++ bin/install-to-home-local-bin.sh
@@ -6,22 +6,22 @@ LOCAL_BIN="$(realpath "`dirname "$0"`/../.local/bin")"
 HOME_LOCAL_BIN="$HOME/.local/bin"
 
 if [ -d "$HOME_LOCAL_BIN" ]; then
-  echo "$HOME_LOCAL_BIN exists"
+    echo "$HOME_LOCAL_BIN exists"
 else
-  echo "$HOME_LOCAL_BIN not exists"
-  exit 1
+    echo "Please create $HOME_LOCAL_BIN"
+    exit 1
 fi
 
 if [ -n "$(echo "$PATH" | grep "$HOME_LOCAL_BIN")" ]; then
-  echo "$HOME_LOCAL_BIN is in your PATH"
+    echo "$HOME_LOCAL_BIN is in your PATH"
 else
-  echo "$HOME_LOCAL_BIN is not in your PATH"
-  exit 1
+    echo "Please add $HOME_LOCAL_BIN to your PATH"
+    exit 1
 fi
 
 for local_bin in $LOCAL_BIN/*; do
-  home_local_bin="$HOME_LOCAL_BIN/`basename "$local_bin" .sh`"
+    home_local_bin="$HOME_LOCAL_BIN/`basename "$local_bin" .sh`"
 
-  echo "Symlink $local_bin to $home_local_bin"
-  ln -sfT "$local_bin" "$home_local_bin"
+    echo "Symlink $local_bin to $home_local_bin"
+    ln -sfT "$local_bin" "$home_local_bin"
 done
