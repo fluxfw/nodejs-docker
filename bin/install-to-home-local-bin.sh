4421572bc11baa8acec090d7aa7a89bf8f2c4ce0
fluxfw <fluxfw@fluxlabs.ch>
2022-03-10 10:47:56 +0100

    install-project-to-home-local-bin

--- bin/install-to-home-local-bin.sh
+++ bin/install-to-home-local-bin.sh
@@ -2,26 +2,9 @@
 
 set -e
 
-LOCAL_BIN="$(realpath "`dirname "$0"`/../.local/bin")"
-HOME_LOCAL_BIN="$HOME/.local/bin"
-
-if [ -d "$HOME_LOCAL_BIN" ]; then
-    echo "$HOME_LOCAL_BIN exists"
-else
-    echo "Please create $HOME_LOCAL_BIN"
+if [ -z `command -v install-project-to-home-local-bin` ]; then
+    echo "Please install flux-docker-utils"
     exit 1
 fi
 
-if [ -n "$(echo "$PATH" | grep "$HOME_LOCAL_BIN")" ]; then
-    echo "$HOME_LOCAL_BIN is in your PATH"
-else
-    echo "Please add $HOME_LOCAL_BIN to your PATH"
-    exit 1
-fi
-
-for local_bin in $LOCAL_BIN/*; do
-    home_local_bin="$HOME_LOCAL_BIN/`basename "$local_bin" .sh`"
-
-    echo "Symlink $local_bin to $home_local_bin"
-    ln -sfT "$local_bin" "$home_local_bin"
-done
+install-project-to-home-local-bin "`dirname "$0"`/.."
